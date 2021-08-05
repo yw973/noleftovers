6d1eabafed3ce8256fc0ee401cd741682dfc4f79
Yinglu Wang <yingluwang@Yinglus-MBP.lan>
2021-08-04 22:34:42 -0400

    implemented food

--- app/src/main/java/com/example/noleftovers/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/noleftovers/ui/home/HomeViewModel.java
@@ -4,6 +4,10 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
@@ -15,5 +19,38 @@ public class HomeViewModel extends ViewModel {
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public class Food {
+        String name;
+        int amount;
+        Date addedDate;
+        Date dateExpired;
+        Date daysLeft;
+        List<String> categories;
+
+        public Food(String name, int amount, List<String> categories, Date dateExpired) {
+            this.name = name;
+            this.amount = amount;
+            this.categories = categories;
+            this.addedDate = getToday();
+            this.dateExpired = dateExpired;
+        }
+
+        private Date getToday() {
+            Calendar calendar = Calendar.getInstance();
+            return calendar.getTime();
+        }
+
+        /* Return the number of days remaining before expiration. */
+        public int getDaysLeft() {
+            long durationL = dateExpired.getTime()-getToday().getTime();
+            int duration = (int) ((durationL / (1000 * 60 * 60 * 24)));
+            return duration;
+        }
+
+        public void updateAmount(int change) {
+            amount += change;
+        }
     }
 }
